fba45c9b5d8aac4fcf75113ff7c1e956548dfaae
soyeonysoss <qkrdus23@gmail.com>
2022-01-21 14:43:30 +0900

    css 수정

--- JS/quotes.js
+++ JS/quotes.js
@@ -1,9 +1,5 @@
 const quotes = [
     {
-        quote: "It takes 20 years to build a reputation and five minutes to ruin it. If you think about that, you'll do things differently.",
-        author: "Warren Buffett",
-    },
-    {
         quote: "It is possible to store the mind with a million facts and still be entirely uneducated.",
         author: "Alec Bourne",
     },
@@ -14,10 +10,6 @@ const quotes = [
     {
         quote: "If you want to see what children can do, you must stop giving them things.",
         author: "Norman Douglas",
-    },
-    {
-        quote: "The Chinese use two brush strokes to write the word 'crisis.' One brush stroke stands for danger; the other for opportunity. In a crisis, be aware of the danger - but recognize the opportunity.",
-        author: "John F. Kennedy",
     },
     {
         quote: "Eighty percent of success is showing up.",
